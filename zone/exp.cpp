0098909ac890704459816175b97d51f7738775ee
carolus21rex <85852042+carolus21rex@users.noreply.github.com>
2024-08-25 11:44:04 -0400

    Fixed SQL related crash bug
    Modified rules from line 621 to 630 from RuleI to RuleR

--- zone/exp.cpp
+++ zone/exp.cpp
@@ -619,16 +619,16 @@ uint64 Client::AddPowersourceExp(uint64 exp_to_add, int conlevel) {
     int cap = 0;
     switch (conlevel) {
         case ConsiderColor::Red:
-            cap = static_cast<int>(RuleI(Character, FloatingExperiencePercentCapPerRedKill));
+            cap = static_cast<int>(RuleR(Character, FloatingExperiencePercentCapPerRedKill));
             break;
         case ConsiderColor::Yellow:
-            cap = static_cast<int>(RuleI(Character, FloatingExperiencePercentCapPerYellowKill));
+            cap = static_cast<int>(RuleR(Character, FloatingExperiencePercentCapPerYellowKill));
             break;
         case ConsiderColor::White:
-            cap = static_cast<int>(RuleI(Character, FloatingExperiencePercentCapPerWhiteKill));
+            cap = static_cast<int>(RuleR(Character, FloatingExperiencePercentCapPerWhiteKill));
             break;
         case ConsiderColor::DarkBlue:
-            cap = static_cast<int>(RuleI(Character, FloatingExperiencePercentCapPerBlueKill));
+            cap = static_cast<int>(RuleR(Character, FloatingExperiencePercentCapPerBlueKill));
             break;
         case ConsiderColor::LightBlue:
             cap = static_cast<int>(RuleR(Character, FloatingExperiencePercentCapPerLightBlueKill));
